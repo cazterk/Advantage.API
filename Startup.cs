ce1c90104563aafb29c5d20a694edb20e55bca32
Cephas Zulu <48860145+cazterk@users.noreply.github.com>
2021-06-17 19:29:48 +0200

    adde CorsPolicy

--- Startup.cs
+++ Startup.cs
@@ -28,6 +28,14 @@ namespace Advantage.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors(opt =>
+            {
+                opt.AddPolicy("CorsPolicy",
+                c => c.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod()
+                );
+            });
+
+
             _connectionString = Configuration["secretconnectionString"];
 
 
@@ -46,6 +54,7 @@ namespace Advantage.API
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseCors("CorsPolicy");
             }
             else
             {
@@ -60,6 +69,6 @@ namespace Advantage.API
             ));
 
         }
-        
+
     }
 }

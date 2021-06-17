2f3a4b444ab55825ac339855953fb8da36417ab1
Cephas Zulu <48860145+cazterk@users.noreply.github.com>
2021-06-17 15:56:07 +0200

    added  customercontroller

---
+++ Controllers/CustomerController.cs
@@ -0,0 +1,51 @@
+using Advantage.API.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Linq;
+
+namespace Advantage.API.Demo.Controllers
+{
+    [Route("api/[controller]")]
+    public class CustomerController : Controller
+    {
+        private readonly APIContext _ctx;
+
+        public CustomerController(APIContext ctx)
+        {
+            _ctx = ctx;
+        }
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var data = _ctx.Customers.OrderBy(c => c.Id);
+
+            return Ok(data);
+        }
+
+
+
+        [HttpGet("{id}", Name = "GetCustomer")]
+        public Customer Get(int id)
+        {
+            return _ctx.Customers.Find(id);
+        }
+
+        // POST api/values
+        [HttpPost]
+        public IActionResult Post([FromBody] Customer customer)
+        {
+            if (customer == null)
+            {
+                return BadRequest();
+            }
+
+            _ctx.Customers.Add(customer);
+            _ctx.SaveChanges();
+
+            return CreatedAtRoute("GetCustomer", new { id = customer.Id }, customer);
+        }
+
+    }
+
+
+}
